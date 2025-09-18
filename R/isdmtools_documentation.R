3fb698133b1e4905fe488e147de77e00616086ce
sodeidelphonse <sdidelphonse@gmail.com>
2025-09-18 14:21:12 +0100

    Separating the package documentation

---
+++ R/isdmtools_documentation.R
@@ -0,0 +1,13 @@
+# --- Package documentation ---
+#' @title A Toolkit for Integrated Species Distribution Models
+#'
+#' @description
+#' `isdmtools` provides a set of tools for preparing, analyzing and visualizing spatial data for integrated species distribution models (ISDMs).
+#' It is designed to help users prepare multisource spatial point datasets for block cross-validation, with a focus on Bayesian inference.
+#' It also analyse the habitat suitability from joint model predictions and map the results. The package also provides a holistic view of model performance
+#' by computing a comprehensive evaluation metrics for the joint model, including ROC-based and continuous-outcome weighted composite scores.
+#'
+#' @name isdmtools-package
+#' @aliases isdmtools
+#' @keywords internal
+"_PACKAGE"
